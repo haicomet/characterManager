d2835e3174e8adb2c3cf6fb5ae5b2d46de2062e3
hailsom <hailiassommerville@gmail.com>
2025-04-14 21:05:14 -0400

    Initial Commit

---
+++ characterManager/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  characterManager
+//
+//  Created by Hailia Sommerville on 4/14/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
