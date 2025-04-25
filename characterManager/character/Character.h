81f17b99912555a459e1bd2a5a0866a3e0712384
hailsom <hailiassommerville@gmail.com>
2025-04-25 00:40:26 -0400

    character completed (more or less)

--- characterManager/character/Character.h
+++ characterManager/character/Character.h
@@ -4,22 +4,29 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 using namespace std;
 
 class Character{
 private:
-    string name, guild, background, identity;
+    string name, guild, background, identity, race;
     int grit, logic, confidence;
+    int xp, level, xpGained, toLvlUp;
 public:
     Character(string);
     void setGuild(string);
     void setBg();
     void setIdentity();
-    void setStats(int,int,int);
+    void setRace();
+    void boostStats(int,int,int);
     int getGrit();
     int getLogic();
     int getConfidence();
     void displayChar();
+    void levelUp();
+    friend void plusXP(Character&);
+    void saveCharacter();
+    void loadCharacter();
     
 };
 
