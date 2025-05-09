d3463df559e6fe3d3ab1b7e2a7a9a18385616c2b
Dylan Yagninim <86193793+D3su0shi@users.noreply.github.com>
2025-05-09 18:44:07 -0400

    Update popQuiz.cpp

--- characterManager/enemy/popQuiz/popQuiz.cpp
+++ characterManager/enemy/popQuiz/popQuiz.cpp
@@ -3,11 +3,16 @@
 
 PopQuiz::PopQuiz()
   : Enemy("Pop Quiz", 20, 2, {
-        {"Is C++ an object-oriented language? (Y/N)", "a"},
-        {"True or False: In C++, 0 is treated as false. (T/F)", "a"},
-        {"Do you need <vector> to use std::vector? (Y/N)", "a"},
-        {"True or False: A function in C++ can directly return multiple values. (T/F)", "a"},
-        {"Is `int main()` mandatory in every C++ program? (Y/N)", "a"}
+        {"Is C++ an object-oriented language? (Y/N)", "Y"},
+        {"True or False: In C++, 0 is treated as false. (T/F)", "T"},
+        {"Do you need <vector> to use std::vector? (Y/N)", "Y"},
+        {"True or False: A function in C++ can directly return multiple values. (T/F)", "F"},
+        {"Is `int main()` mandatory in every C++ program? (Y/N)", "Y"},
+        {"Which of these initializes a string?\nA) string s = \"Hello\";\nB) s = String(\"Hello\");\nC) String s = Hello;", "A"},
+        {"What does 'endl' do in cout?\nA) Ends the file\nB) Adds a newline\nC) Ends logic", "B"},
+        {"Which of the following is a correct if-statement?\nA) if x > 3\nB) if (x > 3)\nC) if x > 3 then", "B"},
+        {"True or False: You must always use using namespace std; to use cout. (T/F)", "F"},
+        {"What value does an uninitialized int variable contain?\nA) 0\nB) Garbage/Unknown\nC) NULL", "B"}
     })
 {xpGained = 3;}
 
